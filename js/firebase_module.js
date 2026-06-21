ec2c87b97bff6ebde952507c44662fb993024891
Talles andré <tallesandre15@gmail.com>
2026-06-20 22:38:30 -0300

    centralizar cinfiguração do firebase

--- js/firebase_module.js
+++ js/firebase_module.js
@@ -1,12 +1,8 @@
-
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-analytics.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-firestore.js";
+import { collection, addDoc, getDocs, query, orderBy } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-firestore.js";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyDmMnWK_NZuh1ZC5W5Pej-oJt6uAvHKnbw",
     authDomain: "learnig-firebase-535ad.firebaseapp.com",
@@ -17,6 +13,8 @@ const firebaseConfig = {
     measurementId: "G-6B1QCZL2YB"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const db = getFirestore(app);
+
+export { app, db, collection, addDoc, getDocs, query, orderBy };
