a0e51e866b2db0f0a57f2a52819268b1983774e7
Talles andré <tallesandre15@gmail.com>
2026-06-20 22:38:39 -0300

    fazer pagina de eventos consumir dados do firebase

---
+++ js/eventos.js
@@ -0,0 +1,47 @@
+import {db, collection, addDoc, getDocs, query, orderBy } from "./firebase_module.js";
+
+const container = document.querySelector('.main_content');
+
+async function carregarEventos() {
+    const q = query(collection(db, "eventos"), orderBy("data"));
+    const snapshot = await getDocs(q);
+
+    snapshot.forEach((doc) => {
+        const evento = doc.data();
+        container.insertAdjacentHTML('beforeend', criarCardHTML(evento));
+    });
+}
+
+function criarCardHTML(evento) {
+    const statusClasse = {
+        "Em Breve": "embreve",
+        "Acontecendo": "acontecendo",
+        "Realizado": "realizado"
+    }[evento.status] || "";
+
+    return `
+        <div class="event_card">
+            <h3>${evento.titulo}</h3>
+            <div class="pill ${statusClasse}">
+                <p>${evento.status}</p>
+            </div>
+            <p>${evento.descricao}</p>
+            <div>
+                <div>
+                    <img src="../assets/icons/calendario.png" alt="">
+                    <p>${evento.data}</p>
+                </div>
+                <div>
+                    <img src="../assets/icons/relogio.png" alt="">
+                    <p>${evento.hora_inicio} - ${evento.hora_termino}</p>
+                </div>
+                <div>
+                    <img src="../assets/icons/mapas-e-bandeiras.png" alt="">
+                    <p>${evento.local}</p>
+                </div>
+            </div>
+        </div>
+    `;
+}
+
+carregarEventos();
